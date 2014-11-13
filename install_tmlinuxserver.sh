707ac15a51c51e208162c2ccf6659354d0f265e5
elvito <elvito@users.noreply.github.com>
2014-11-13 13:38:30 +0100

    Update install_tmlinuxserver.sh

--- install_tmlinuxserver.sh
+++ install_tmlinuxserver.sh
@@ -2,9 +2,14 @@
 sudo apt-get update
 sudo apt-get install git
 if [ -d ~/tm-linux-server-scripte ];then
+	echo "Der Installationsordner für die Scripte ist bereits vorhanden beginne in 5 Sekunden mit dem synchronisieren"
+	sleep 5s
 	cd ~/tm-linux-server-scripte
 	git clone git://https://github.com/elvito/tm-linux-server-installhelper.git .
 else
+	echo "Der Installationsordner für die Scripte ist noch nicht vorhanden!"
+	echo "Die Scripte werden unter ~/tm-linux-server-scripte/ gespeichert"
+	sleep 5s
 	mkdir ~/tm-linux-server-scripte
 	cd ~/tm-linux-server-scripte
 	git clone git://https://github.com/elvito/tm-linux-server-installhelper.git .
