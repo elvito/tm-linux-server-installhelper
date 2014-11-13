0c7fd28bbd3f1da0a679179b7828ec0889d4ab65
elvito <elvito@users.noreply.github.com>
2014-11-13 13:58:04 +0100

    Update install_tmlinuxserver.sh
    Falsche Adresse bei git geändert. Jetzt gehts :)

--- install_tmlinuxserver.sh
+++ install_tmlinuxserver.sh
@@ -5,13 +5,13 @@ if [ -d ~/tm-linux-server-scripte ];then
 	echo "Der Installationsordner für die Scripte ist bereits vorhanden beginne in 5 Sekunden mit dem synchronisieren"
 	sleep 5s
 	cd ~/tm-linux-server-scripte
-	git clone git://https://github.com/elvito/tm-linux-server-installhelper.git .
+	git clone git://github.com/elvito/tm-linux-server-installhelper.git .
 else
 	echo "Der Installationsordner für die Scripte ist noch nicht vorhanden!"
 	echo "Die Scripte werden unter ~/tm-linux-server-scripte/ gespeichert"
 	sleep 5s
 	mkdir ~/tm-linux-server-scripte
 	cd ~/tm-linux-server-scripte
-	git clone git://https://github.com/elvito/tm-linux-server-installhelper.git .
+	git clone git://github.com/elvito/tm-linux-server-installhelper.git .
 fi
 exit 0
