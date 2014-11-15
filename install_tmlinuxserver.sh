00be162ec999aaefe8c9228c8246137899b3fb75
elvito <elvito@users.noreply.github.com>
2014-11-15 16:43:30 +0100

    Update install_tmlinuxserver.sh

--- install_tmlinuxserver.sh
+++ install_tmlinuxserver.sh
@@ -108,7 +108,7 @@ case "$choice" in
 	;;
 
 	"Einrichtung von iptables (Firewall)")
-	$DIALOG --msgbox "Diese Option ist noch nicht implementiert" 0 40
+	$DIALOG --msgbox "Diese Option ist noch nicht implementiert" 5 0
 	$DIALOG --clear
 	;;
 
