c2b95e5a4adb82482acc31db62284ff898ef07d1
elvito <vitus.hanemann@elvito.de>
2014-11-15 15:53:00 +0100

    Code bereinigt

--- install_tmlinuxserver.sh
+++ install_tmlinuxserver.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-#### Zunächst mal die Basics installieren ####
+
+#### Zunächst mal die absoluten Basics installieren ####
 apt-get update
 apt-get install git dialog
+
+#### Definiere verwendete verwendete Programme #### 
+DIALOG=which dialog
+APTGET=which apt-get
+CP=which cp
+
+#### Beginn der Schleife ####
 while true; do
+
 #### Die Variable "choice" wird definiert ####
-choice=`dialog --menu \
+choice=`DIALOG --menu \
 	"Auswahl" 0 0 0 \
 	"Ubuntu Systemupdate" "" \
 	"Installation von tm-linux-server-installhelper" "" \
@@ -13,96 +22,93 @@ choice=`dialog --menu \
 	"Einrichtung von Samba" "" \
 	"Einrichtung von iptables (Firewall)" "" \
 	"Dieses Menü beenden" "" 3>&1 1>&2 2>&3`
-#	dialog --clear
-# 	dialog --yesno "Bestätigen Sie Ihre Auswahl: $choice" 0 0 
-#	dialog --clear
-#	clear
+	DIALOG --clear
+	DIALOG --yesno "Bestätigen Sie Ihre Auswahl: $choice" 0 0
+	DIALOG --clear
+	clear
 
 #### Weiterverarbeitung der Variablen "choice" ####
-case "$choice" in 
+case "$choice" in
 
-   "Ubuntu Systemupdate")
-        dialog --clear
-	apt-get update
-	apt-get upgrade
-	apt-get dist-upgrade
-	apt-get autoremove
-	./install_tmlinuxserver.sh
-   ;;
-   
-   "Installation von tm-linux-server-installhelper")
-        dialog --clear
-	   if [ -d ~/tm-linux-server-scripte ]
-	      then
-	         dialog --infobox "Der Installationsordner für die Scripte ist bereits vorhanden... Beginne mit dem synchronisieren" 5 40
-		 sleep 5s
-		 dialog --clear
-		 cd ~/tm-linux-server-scripte
-		 git clone git://github.com/elvito/tm-linux-server-installhelper.git .
-		 ./install_tmlinuxserver.sh
-	      else
-		 dialog --infobox "Der Installationsordner für die Scripte wird neu angelegt, beginne in 5 Sekunden mit dem synchronisieren" 5 40
-		 sleep 5s
-		 dialog --clear
-		 dialog --msgbox "Die Scripte werden unter ~/tm-linux-server-scripte/ gespeichert"
-		 mkdir ~/tm-linux-server-scripte
-		 cd ~/tm-linux-server-scripte
-		 git clone git://github.com/elvito/tm-linux-server-installhelper.git .
-		 dialog --msgbox "Die Scripte wurden erfolgreich im Ordner ~/tm-linux-server-scripte/ installiert :)" 
-		 ./install_tmlinuxserver.sh
-	    fi
-   ;;
-   
-   "Installation von TM Linux Server")
-	dialog --clear
+	"Ubuntu Systemupdate")
+	DIALOG --clear
+	APTGET update
+	APTGET upgrade
+	APTGET dist-upgrade
+	APTGET autoremove
+	;;
+
+	"Installation von tm-linux-server-installhelper")
+	DIALOG --clear
+		if [ -d ~/tm-linux-server-scripte ]
+			then
+				DIALOG --infobox "Der Installationsordner für die Scripte ist bereits vorhanden... Beginne mit dem synchronisieren" 5 40
+		 		sleep 5s
+		 		DIALOG --clear
+		 		cd ~/tm-linux-server-scripte
+		 		git clone git://github.com/elvito/tm-linux-server-installhelper.git .
+			else
+		 		DIALOG --infobox "Der Installationsordner für die Scripte wird neu angelegt, beginne in 5 Sekunden mit dem synchronisieren" 5 40
+		 		sleep 5s
+		 		DIALOG --clear
+		 		DIALOG --msgbox "Die Scripte werden unter ~/tm-linux-server-scripte/ gespeichert"
+		 		mkdir ~/tm-linux-server-scripte
+		 		cd ~/tm-linux-server-scripte
+		 		git clone git://github.com/elvito/tm-linux-server-installhelper.git .
+		 		DIALOG --msgbox "Die Scripte wurden erfolgreich im Ordner ~/tm-linux-server-scripte/ installiert :)" 
+	    	fi
+	;;
+
+	"Installation von TM Linux Server")
+	DIALOG --clear
 	cd ~/tm-linux-server-scripte
 	./tm-linux-server-vorbereitungsscript.sh
-	dialog --msgbox "Der TM Linux Server wurde installiert :)" 5 40
-	dialog --infobox "Überprüfung ob der FastObjectServer läuft beginnt in 5 Sekunden" 5 40
+	DIALOG --msgbox "Der TM Linux Server wurde installiert :)" 5 40
+	DIALOG --infobox "Überprüfung ob der FastObjectServer läuft beginnt in 5 Sekunden" 5 40
 	sleep 5s
-	dialog --clear
+	DIALOG --clear
 	clear
 	/etc/init.d/poet status
 	echo -e "\n\nSie sollten eine PID und \"running\" sehen\nBitte eine beliebige Taste drücken"
-        read -sn1	
-	dialog --msgbox "Der TM Linux Server wurde installiert :)" 5 40
-   ;;
-   
-   "Vollständiges Entfernen von TM Linux Server")
+	read -sn1	
+	DIALOG --msgbox "Der TM Linux Server wurde installiert :)" 5 40
+	;;
+
+	"Vollständiges Entfernen von TM Linux Server")
 	/opt/turbomed/TM_setup -rm
 	#rm -f /opt/FastObject* 
-	dialog --infobox "Löschen von TM Linux Server abgeschlossen" 5 40
-	dialog --clear
-   ;;
-   
-   "Einrichtung von Samba")
-	if [ -d ~/tm-linux-server-scripte/ ]
-	     then
-	        dialog --msgbox "Installiere die angepasste smb.conf und starte anschließend Samba neu" 5 40
-	        sleep 5s
-                cp -b ~/tm-linux-server-scripte/smb.conf /etc/samba/
-                chmod 644 /etc/samba/smb.conf
-                service samba restart
-	        dialog --infobox "Einrichtung des angepassten smb.conf abgeschlossen, Samba wurde neu gestartet" 5 40
-	        dialog --clear
-	      else 	
-		dialog --msgbox "Installieren Sie zuerst tm-linux-server-installhelper"
-		dialog --clear
-	fi
-   ;;
-			
-   "Einrichtung von iptables (Firewall)")
-      dialog --msgbox "Diese Option ist noch nicht implementiert"
-#      dialog --clear
-   ;;
-   
-   "Dieses Menü beenden")
-      exit 0
-   ;;
-   
-   *)
-      exit 1
-   ;;
-   
+	DIALOG --infobox "Löschen von TM Linux Server abgeschlossen" 5 40
+	DIALOG --clear
+	;;
+
+	"Einrichtung von Samba")
+		if [ -d ~/tm-linux-server-scripte/ ]
+			then
+				DIALOG --msgbox "Installiere die angepasste smb.conf und starte anschließend Samba neu" 5 40
+				sleep 5s
+				CP -b ~/tm-linux-server-scripte/smb.conf /etc/samba/
+				chmod 644 /etc/samba/smb.conf
+				service samba restart
+				DIALOG --infobox "Einrichtung des angepassten smb.conf abgeschlossen, Samba wurde neu gestartet" 5 40
+				DIALOG --clear
+			else
+				DIALOG --msgbox "Installieren Sie zuerst tm-linux-server-installhelper"
+				DIALOG --clear
+		fi
+	;;
+
+	"Einrichtung von iptables (Firewall)")
+	DIALOG --msgbox "Diese Option ist noch nicht implementiert"
+	#DIALOG --clear
+	;;
+
+	"Dieses Menü beenden")
+	exit 0
+	;;
+
+	*)
+	exit 1
+	;;
+
 esac
 done
