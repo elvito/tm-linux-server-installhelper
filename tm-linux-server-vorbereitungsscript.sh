f19397acfc480022c56ee40e4561ee2b2dd9cf3d
elvito <elvito@users.noreply.github.com>
2014-11-15 17:22:59 +0100

    Update tm-linux-server-vorbereitungsscript.sh

--- tm-linux-server-vorbereitungsscript.sh
+++ tm-linux-server-vorbereitungsscript.sh
@@ -4,16 +4,20 @@
 if [ -f ~/Downloads/TMLinux-Version*.zip ]
 	then
 		clear	
-		echo "Die Turbomedinstallationsdatei scheint vorhanden zu sein...fahre fort mit der Vorbereitung <ENTER>"
-		read -sn1
-
-		# Zusätzliche benötigte Pakete nachinstallieren
+		$DIALOG --msgbox "Die Turbomedinstallationsdatei scheint vorhanden zu sein...fahre fort mit der Vorbereitung" 5 40
+		$DIALOG --clear
 		clear
-		echo "Installation zusätzlich benötigter Pakete <ENTER>"
-		read -sn1
+		
+		# Zusätzliche benötigte Pakete nachinstallieren
+		$DIALOG --msgbox "Installation zusätzlich benötigter Pakete" 5 40
+		$DIALOG --clear
+		clear
 		apt-get install gksu mc openssh-server samba samba-doc tdb-tools libc6:i386 libssl0.9.8:i386 libgcc1:i386 libstdc++6:i386 
 
 		#Entpacken der TM Installationsdatei
+		$DIALOG --msgbox "Entpacken der TM Installationsdatei" 5 40
+		$DIALOG --clear
+		clear
 		unzip -d ~/Downloads ~/Downloads/TMLinux-Version*.zip
 
 		# Ausführbarmachen der TM_setup & TM_update
@@ -21,14 +25,14 @@ if [ -f ~/Downloads/TMLinux-Version*.zip ]
 		chmod 755 ~/Downloads/TMWin/linux/bin/TM_update
 
 		# Installieren TM Server
-		echo "Installiere Turbomed Server für Linux <ENTER>"
-		read -sn1
+		$DIALOG --msgbox "Installiere Turbomed Server für Linux" 5 40
+		$DIALOG --clear
+		clear
 		~/Downloads/TMWin/linux/bin/TM_setup -i
 	else 
-		clear	
-		echo "Installationsdatei für den Linux Turbomed Server ist nicht im Downloadordner!!! Bitte kopieren Sie die gezippte TM Installationsdatei in den Ordner ~/Downloads <ENTER>"
-		read -sn1
+		$DIALOG --msgbox "Installationsdatei für den Linux Turbomed Server ist nicht im Downloadordner!!! Bitte kopieren Sie die gezippte TM Installationsdatei in den Ordner ~/Downloads" 5 40
+		$DIALOG --clear
+		clear
 		exit 1
 fi 
 exit 0
-
