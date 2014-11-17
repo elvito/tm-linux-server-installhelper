9f3bba0a368f5848f6415b79981a2aced6c5ef6d
elvito <elvito@users.noreply.github.com>
2014-11-17 15:26:30 +0100

    Update tm-linux-server-vorbereitungsscript.sh
    Fehlerkorrektur

--- installerscripts/tm-linux-server-vorbereitungsscript.sh
+++ installerscripts/tm-linux-server-vorbereitungsscript.sh
@@ -2,66 +2,74 @@
 
 DIALOG=dialog
 
-# Bedingungen prüfen
-if [ -f ~/Downloads/TMLinux-Version*.zip ]
+# Zunächst mal prüfen ob Turbomed schon installiert ist
+if [ -d /opt/turbomed ]
 	then
-		# TMLinux-Version*.zip ist vorhanden!		
-		clear	
-		$DIALOG --msgbox "Die Turbomedinstallationsdatei scheint vorhanden zu sein...fahre fort mit der Vorbereitung" 0 0
-		$DIALOG --clear
-		clear
+		#Zurück zum Installer
+		$DIALOG --msgbox "Turbomed ist bereits installiert!"
+		$DIALOG --msgbox "Löschen Sie zuerst die alte Installation"
+		exit 0
+	else
+		# Bedingungen prüfen
+		if [ -f ~/Downloads/TMLinux-Version*.zip ]
+			then
+				# TMLinux-Version*.zip ist vorhanden!		
+				clear	
+				$DIALOG --msgbox "Die Turbomedinstallationsdatei scheint vorhanden zu sein...fahre fort mit der Vorbereitung" 0 0
+				$DIALOG --clear
+				clear
 		
-		# Benötigte Pakete nachinstallieren
-		$DIALOG --msgbox "Installation zusätzlich benötigter Pakete" 0 0
-		$DIALOG --clear
-		clear
-		apt-get install gksu mc openssh-server samba samba-doc tdb-tools libc6:i386 libssl0.9.8:i386 libgcc1:i386 libstdc++6:i386 
+				# Benötigte Pakete nachinstallieren
+				$DIALOG --msgbox "Installation zusätzlich benötigter Pakete" 0 0
+				$DIALOG --clear
+				clear
+				apt-get install gksu mc openssh-server samba samba-doc tdb-tools libc6:i386 libssl0.9.8:i386 libgcc1:i386 libstdc++6:i386
 
-		# Entpacken der TM Installationsdatei
-		$DIALOG --msgbox "Entpacken der CGM TM Installationsdatei" 0 0
-		$DIALOG --clear
-		clear
-		unzip -d ~/Downloads ~/Downloads/TMLinux-Version*.zip
+				# Entpacken der TM Installationsdatei
+				$DIALOG --msgbox "Entpacken der CGM TM Installationsdatei" 0 0
+				$DIALOG --clear
+				clear
+				unzip -d ~/Downloads ~/Downloads/TMLinux-Version*.zip
 
-		# Ausführbarmachen der TM_setup & TM_update
-		chmod 755 ~/Downloads/TMWin/linux/bin/TM_setup
-		chmod 755 ~/Downloads/TMWin/linux/bin/TM_update
+				# Ausführbarmachen der TM_setup & TM_update
+				chmod 755 ~/Downloads/TMWin/linux/bin/TM_setup
+				chmod 755 ~/Downloads/TMWin/linux/bin/TM_update
 
-		# Installieren des TM Server
-		$DIALOG --msgbox "Installiere Turbomed Server für Linux" 0 0
-		$DIALOG --clear
-		clear
-		~/Downloads/TMWin/linux/bin/TM_setup -i
+				# Installieren des TM Server
+				$DIALOG --msgbox "Installiere Turbomed Server für Linux" 0 0
+				$DIALOG --clear
+				clear
+				~/Downloads/TMWin/linux/bin/TM_setup -i
 		
-		# Test der Installation
-			if [ -f /etc/init.d/poetd ]
-		  		then		
-		    			$DIALOG --infobox "Überprüfung ob der FastObjectServer funktioniert..." 0 0
-		    			sleep 3s
-		    			$DIALOG --clear
-		    			clear
-		    			/etc/init.d/poetd start
-		    			sleep 2s
-		    			clear
-		    			/etc/init.d/poetd status
-		    			echo -e "\n\nSie sollten eine PID und \"running\" sehen\nBitte eine beliebige Taste drücken"
-		    			read -sn1
-				else
-					$DIALOG --msgbox "Irgendwas ist schief gelaufen..." 0 0
-					$DIALOG --msgbox "Melden Sie sich mit einer Fehlerbeschreibung im Forum" 0 0
-					exit 1
-			fi	
+				# Test der Installation
+					if [ -f /etc/init.d/poetd ]
+				  		then
+				    			$DIALOG --infobox "Überprüfung ob der FastObjectServer funktioniert..." 0 0
+				    			sleep 3s
+				    			$DIALOG --clear
+				    			clear
+				    			/etc/init.d/poetd start
+				    			sleep 2s
+				    			clear
+				    			/etc/init.d/poetd status
+				    			echo -e "\n\nSie sollten eine PID und \"running\" sehen\nBitte eine beliebige Taste drücken"
+				    			read -sn1
+						else
+							$DIALOG --msgbox "Irgendwas ist schief gelaufen..." 0 0
+							$DIALOG --msgbox "Melden Sie sich mit einer Fehlerbeschreibung im Forum" 0 0
+							exit 1
+					fi	
 
-# Bedingungen prüfen wird hier fortgesetzt
-	else 
-		    $DIALOG --msgbox "Installationsdatei für den Linux Turbomed Server ist nicht im Downloadordner!" 0 0
-		    $DIALOG --msgbox "Bitte kopieren Sie die gezippte TM Installationsdatei in den Ordner ~/Downloads" 0 0
-		    $DIALOG --clear
-		    clear
-		    exit 1
-		
-fi 
+		# Bedingungen prüfen wird hier fortgesetzt
+			else 
+		    		$DIALOG --msgbox "Installationsdatei für den Linux Turbomed Server ist nicht im Downloadordner!" 0 0
+		    		$DIALOG --msgbox "Bitte kopieren Sie die gezippte TM Installationsdatei in den Ordner ~/Downloads" 0 0
+		    		$DIALOG --clear
+		    		clear
+		    		exit 1
 
+		fi 
+fi
 # Abschliessende Mitteilung an den Benutzer
 if [ -d /opt/turbomed ]
 	then
