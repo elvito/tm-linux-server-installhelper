8a076fd5cc637304d539c34846b790612acf18c1
elvito <vitus.hanemann@elvito.de>
2014-11-18 13:46:40 +0100

    Updater für die Installerdatei hinzugefügt (jeweils für master und testing). Installer wird nach ~/ kopiert

---
+++ update_tm-installer-testing.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# alten Installer löschen
+if [ -f ~/install_tmlinuxserver.sh ]
+	then
+		rm -f ~/install_tmlinuxserver.sh
+		clear
+		echo "Der alte Installer wurde entfernt, bitte beliebige Taste drücken um fortzufahren"
+		read -sn1
+	else
+		clear
+		echo "Kein alter Installer gefunden, fahre in 5 Sekunden fort mit dem Update"
+		sleep 5s
+fi
+# Herunterladen
+wget -P /tmp/testing -c https://github.com/elvito/tm-linux-server-installhelper/archive/testing.zip
+#Entpacken nach /tmp
+unzip /tmp/testing/testing.zip -d /tmp/testing
+#Den installer nach home kopieren
+cp  /tmp/testing/tm-linux-server-installhelper-testing/install_tmlinuxserver.sh ~/
+# Ausführbar machen
+chmod 755 ~/install_tmlinuxserver.sh
+#Aufräumen nach Install
+rm -rf /tmp/testing*
+#Testen
+if [ -f ~/install_tmlinuxserver.sh ]
+	then
+		clear
+		echo "install_tmlinuxserver.sh wurde aktualisiert"
+		exit 0
+	else
+		clear
+		echo "irgendwas ist schief gelaufen"
+		exit 1
+fi
