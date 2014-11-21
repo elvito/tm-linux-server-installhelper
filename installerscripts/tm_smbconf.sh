8340e740c967988d07d75ab83db78ffe150a6d47
elvito <elvito@users.noreply.github.com>
2014-11-21 12:00:33 +0100

    Update tm_smbconf.sh

--- installerscripts/tm_smbconf.sh
+++ installerscripts/tm_smbconf.sh
@@ -58,7 +58,7 @@ case "$choice" in
 			$DIALOG --msgbox "Melden Sie sich bei Fehlermeldungen mit der exakten Fehlerausgabe im Forum" 0 0
 			$DIALOG --msgbox "Einrichtung des angepassten smb.conf abgeschlossen, starten Sie ggf. Samba neu falls Ihre Freigabe nicht verfügbar ist" 0 0
 			$DIALOG --clear
-			clear		
+			clear
 	;;
 	
 	# Samba Stop	
